0a62f2bb1199166c168ec20101fbe67a9e6b35d6
Robert Matych <dev@regattatropicals.com>
2018-06-20 13:48:06 -0700

    Adjust starter SQL schema

--- initdb.sql
+++ initdb.sql
@@ -2,22 +2,22 @@ CREATE DATABASE IF NOT EXISTS `ptdb`;
 USE `ptdb`;
 
 CREATE TABLE IF NOT EXISTS `ptdb`.`Employee` (
-  `employee_id` INT NOT NULL,
-  `employee_code` VARCHAR(60) NOT NULL,
+  `employee_id` INT NOT NULL AUTO_INCREMENT,
+  `employee_code` VARCHAR(60) NULL,
   `employee_firstname` VARCHAR(20) NOT NULL,
   `employee_lastname` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`employee_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ptdb`.`Packer` (
-  `packer_id` INT NOT NULL,
+  `packer_id` INT NOT NULL AUTO_INCREMENT,
   `employee_id` INT NOT NULL,
   PRIMARY KEY (`packer_id`),
   FOREIGN KEY (`employee_id`) REFERENCES `Employee`(`employee_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ptdb`.`Manager` (
-  `manager_id` INT NOT NULL,
+  `manager_id` INT NOT NULL AUTO_INCREMENT,
   `employee_id` INT NOT NULL,
   PRIMARY KEY (`manager_id`),
   FOREIGN KEY (`employee_id`) REFERENCES `Employee`(`employee_id`)
@@ -28,27 +28,27 @@ CREATE TABLE IF NOT EXISTS `ptdb`.`Credentials` (
   `username` VARCHAR(40) NOT NULL,
   `email` VARCHAR(254) NOT NULL,
   `salt` BINARY(32) NOT NULL,
-  `salted_hash` BINARY(32) NOT NULL,
-  `admin` BOOL NOT NULL,
+  `salted_hash` BINARY(96) NOT NULL,
+  `is_admin` BOOL NOT NULL DEFAULT FALSE,
   PRIMARY KEY (`username`),
   FOREIGN KEY (`employee_id`) REFERENCES `Employee`(`employee_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ptdb`.`Commodity` (
-  `commodity_id` INT NOT NULL,
+  `commodity_id` INT NOT NULL AUTO_INCREMENT,
   `commodity_name` VARCHAR(20) NOT NULL,
   `commodity_packtype` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`commodity_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ptdb`.`Location` (
-  `location_id` INT NOT NULL,
+  `location_id` INT NOT NULL AUTO_INCREMENT,
   `location_name` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`location_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ptdb`.`Line` (
-  `line_id` INT NOT NULL,
+  `line_id` INT NOT NULL AUTO_INCREMENT,
   `location_id` INT NOT NULL,
   `line_name` VARCHAR(40) NOT NULL,
   PRIMARY KEY (`line_id`),
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `ptdb`.`Line` (
 );
 
 CREATE TABLE IF NOT EXISTS `ptdb`.`Position` (
-  `position_id` INT NOT NULL,
+  `position_id` INT NOT NULL AUTO_INCREMENT,
   `position_code` VARCHAR(60) NOT NULL,
   `line_id` INT NOT NULL,
   PRIMARY KEY (`position_id`),
@@ -64,14 +64,14 @@ CREATE TABLE IF NOT EXISTS `ptdb`.`Position` (
 );
 
 CREATE TABLE IF NOT EXISTS `ptdb`.`Roll` (
-  `roll_id` INT NOT NULL,
+  `roll_id` INT NOT NULL AUTO_INCREMENT,
   `position_id` INT NOT NULL,
   PRIMARY KEY (`roll_id`),
   FOREIGN KEY (`position_id`) REFERENCES `Position`(`position_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ptdb`.`Shift` (
-  `shift_id` INT NOT NULL,
+  `shift_id` INT NOT NULL AUTO_INCREMENT,
   `shift_start` DATETIME NOT NULL,
   `shift_end` DATETIME NULL,
   `manager_id` INT NOT NULL,
@@ -82,7 +82,8 @@ CREATE TABLE IF NOT EXISTS `ptdb`.`Shift` (
 );
 
 CREATE TABLE IF NOT EXISTS `ptdb`.`Session` (
-  `session_id` INT NOT NULL,
+  `session_id` INT NOT NULL AUTO_INCREMENT,
   `shift_id` INT NOT NULL,
-  `commodity_id` INT NOT NULL
+  `commodity_id` INT NOT NULL,
+  PRIMARY KEY (`session_id`)
 );
